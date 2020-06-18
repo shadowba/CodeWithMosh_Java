e45ec1ad308959dea963f20ea2dafc7d3651c0ff
shadowba <shadowban995@gmail.com>
2020-06-18 06:38:05 -0700

    Binary tree finished
    Binary tree finished

--- 2_3_Refactoring/src/com/datastructure2/BinarySearchTree.java
+++ 2_3_Refactoring/src/com/datastructure2/BinarySearchTree.java
@@ -1,32 +1,36 @@
 package com.datastructure2;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class BinarySearchTree {
     Node root = null;
     Node current = null;
+    LinkedList<Integer> treeLinkedList = new LinkedList<>();
+    Stack<Integer> valueStack = new Stack<>();
 
     public void insert(int newValue) {
         Node newNode = new Node(newValue);
         current = root;
-        if(root==null){
-            root=newNode;
+        if (root == null) {
+            root = newNode;
             return;
         }
 
         while (current != null) {
             if (newNode.getValue() <= current.getValue()) {
-                if(current.getLeftChild()==null){
+                if (current.getLeftChild() == null) {
                     current.setLeftChild(newNode);
                     break;
-                }
-                else
-                    current=current.getLeftChild();
+                } else
+                    current = current.getLeftChild();
             } else {
-                if(current.getRightChild()==null){
+                if (current.getRightChild() == null) {
                     current.setRightChild(newNode);
                     break;
-                }
-                else
-                    current=current.getRightChild();
+                } else
+                    current = current.getRightChild();
             }
         }
 
@@ -37,19 +41,179 @@ public class BinarySearchTree {
         while (current != null) {
             if (lookValue < current.getValue()) {
                 current = current.getLeftChild();
-            } else if(lookValue>current.getValue()){
+            } else if (lookValue > current.getValue()) {
                 current = current.getRightChild();
-            }else{
+            } else {
                 return true;
             }
         }
         return false;
     }
 
+    public boolean equals(BinarySearchTree newTree) {
+//        treeLinkedList = this.getLinkedList();
+        convertTreeLinkList(root, treeLinkedList);
+        System.out.println(treeLinkedList);
+
+        LinkedList<Integer> newTreeLinkedList = new LinkedList<>();
+        convertTreeLinkList(newTree.getRoot(), newTreeLinkedList);
+//        newTreeLinkedList = newTree.getLinkedList();
+        System.out.println(newTreeLinkedList);
+
+        int maxSize = Math.max(treeLinkedList.size(), newTreeLinkedList.size());
+        for (int i = 0; i < maxSize; i++) {
+
+            if (treeLinkedList.size() != newTreeLinkedList.size() ||
+                    treeLinkedList.toArray()[i] != newTreeLinkedList.toArray()[i])
+                return false;
+        }
+        return true;
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+
+        traverseInOrder(root.getLeftChild());
+        System.out.println(root.getValue());
+        traverseInOrder(root.getRightChild());
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.println(root.getValue());
+        traverseInOrder(root.getLeftChild());
+        traverseInOrder(root.getRightChild());
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+
+        traverseInOrder(root.getLeftChild());
+        traverseInOrder(root.getRightChild());
+        System.out.println(root.getValue());
+    }
+
+    private void convertTreeLinkList(Node root, LinkedList list) {
+        if (root == null)
+            return;
+
+        convertTreeLinkList(root.getLeftChild(), list);
+        convertTreeLinkList(root.getRightChild(), list);
+        list.add(root.value);
+    }
+
+    public LinkedList getLinkedList() {
+        return treeLinkedList;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    private int height(Node root) {
+        if (root == null)
+            return -1;
+
+        if (root.getLeftChild() == null && root.getRightChild() == null)
+            return 0;
+
+        return 1 + Math.max(height(root.getLeftChild()), height(root.getRightChild()));
+    }
+
+    public int min() {
+        return min(root);
+    }
+
+    private int min(Node root) {
+
+        if (root.getLeftChild() == null || root.getRightChild() == null)
+            return root.getValue();
+
+        int left = min(root.leftChild);
+        int right = min(root.rightChild);
+
+        return Math.min(Math.min(left, right), root.getValue());
+    }
+
+//    private void traverseLimitSettings(Node root, double maxLimit, double minLimit) {
+//        if (root == this.root) {
+//            root.maxLimit = Double.POSITIVE_INFINITY;
+//            root.minLimit = Double.NEGATIVE_INFINITY;
+//            return;
+//        }
+//        root.maxLimit = maxLimit;
+//        root.minLimit = minLimit;
+//        if (root.getLeftChild() != null)
+//            traverseLimitSettings(root.getLeftChild(), root.value, root.minLimit);
+//        if (root.getRightChild() != null)
+//            traverseLimitSettings(root.getRightChild(), root.maxLimit, root.value);
+//    }
+
+    public boolean validatingBST() {
+        return validatingBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    private boolean validatingBST(Node root, int maxLimit, int minLimit) {
+//        traverseLimitSettings(this.root,Double.POSITIVE_INFINITY,Double.NEGATIVE_INFINITY);
+//        if(root==null)
+//            return false;
+//        if(root.getLeftChild()!=null)
+//            return validatingBST(root.getLeftChild());
+//        else if(root.getRightChild()!=null)
+//            return validatingBST(root.getRightChild());
+//        else
+//            return (root.value<=root.maxLimit&&root.value>=root.minLimit);
+
+        if (root == null)
+            return false;
+        if (root.value > maxLimit || root.value < minLimit)
+            return false;
+
+        return validatingBST(root.getLeftChild(), root.value + 1, minLimit) && validatingBST(root.getRightChild(), maxLimit, root.value + 1);
+    }
+
+    public void valueFromDistance(Node root, int distance) {
+        if (root == null)
+            return;
+//            return -1;
+
+        if (distance == 0){
+            valueStack.push(root.value);
+            return;
+        }
+        else {
+            valueFromDistance(root.getLeftChild(), distance - 1);
+            valueFromDistance(root.getRightChild(), distance - 1);
+        }
+    }
+
+    Stack<Integer> getValueStack(){
+        return valueStack;
+    }
+
+
     private class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
+        private double maxLimit;
+        private double minLimit;
 
         public Node(int value) {
             this.value = value;
--- 2_3_Refactoring/src/com/datastructure2/Main.java
+++ 2_3_Refactoring/src/com/datastructure2/Main.java
@@ -7,6 +7,20 @@ public class Main {
         for(int item:inputArray){
             BST.insert(item);
         }
-        System.out.println(BST.find(18));
+
+        BinarySearchTree BST2 = new BinarySearchTree();
+        int[] inputArray2 = new int[]{10,5,15,2,1,3};
+        for(int item:inputArray2){
+            BST2.insert(item);
+        }
+//        System.out.println(BST.min());
+//        System.out.println(BST.find(18));
+
+
+        System.out.println(BST.equals(BST2));
+        System.out.println(BST2.validatingBST());
+
+        BST.valueFromDistance(BST.root,2);
+        System.out.println(BST.getValueStack().toString());
     }
 }
