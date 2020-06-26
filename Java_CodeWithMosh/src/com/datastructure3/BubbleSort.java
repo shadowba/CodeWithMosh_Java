cd6d5a5654f09be491bc12a30ead36ec3e581b28
shadowba <shadowban995@gmail.com>
2020-06-26 16:00:33 -0700

    Bubble Sort
    Bubble Sort

--- Java_CodeWithMosh/src/com/datastructure3/BubbleSort.java
+++ Java_CodeWithMosh/src/com/datastructure3/BubbleSort.java
@@ -1,7 +1,24 @@
 package com.datastructure3;
 
 public class BubbleSort {
-    public void sort(int[] array){
-        
+    public void sort(int[] array) {
+        if  (array==null)
+            throw new IllegalArgumentException("Input Array is Null");
+        int cap = array.length - 1;
+
+        while (cap >= 1) {
+            for (int i = 1; i <= cap; i++) {
+                if (array[i - 1] < array[i])
+                    continue;
+                swap(array, i);
+            }
+            cap--;
+        }
+    }
+
+    private void swap(int[] array, int i) {
+        int temp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = temp;
     }
 }
--- Java_CodeWithMosh/src/com/datastructure3/test_BubbleSort.java
+++ Java_CodeWithMosh/src/com/datastructure3/test_BubbleSort.java
@@ -1,7 +1,14 @@
 package com.datastructure3;
 
+import java.util.Arrays;
+
 public class test_BubbleSort {
     public static void main(String[] args) {
+        BubbleSort bubbleSort=new BubbleSort();
+
+        int[] inputArray={1,3,7,4,2,0};
+        bubbleSort.sort(inputArray);
+        System.out.println(Arrays.toString(inputArray));
 
     }
 }
